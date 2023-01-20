3de5262970122b839df87d4c2501ab7cf81fa0ea
Raven737 <prvm79@gmail.com>
2023-01-20 20:28:08 +0200

    fetch was done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-const App = () => (
-  <>
-    <h1>Favourite Movies</h1>
-  </>
-)
+import { useState, useEffect } from 'react';
+
+const App = () => {
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+        setUsers(json)
+      })
+  }, [])
+
+  return (
+    <>
+      <h1>Favourite Movies</h1>
+      {/* {todo && <h2>{todo.title}</h2>} */}
+      {
+        users && users.map(user => {
+          return (
+            <>
+              <h2 key={user.name}>{user.name}</h2>
+              <p key={user.email}>{user.email}</p>
+            </>
+          )
+        })
+      }
+    </>
+  )
+}
 
 export default App;
