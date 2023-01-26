2f8d63307e1fae670b1fd02b969933d45f988dfe
Raven737 <prvm79@gmail.com>
2023-01-26 21:50:04 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
+import { render } from '@testing-library/react';
 import { useState, useEffect } from 'react';
 
 const App = () => {
-  const [users, setUsers] = useState();
+
+  const [films, setFilms] = useState();
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://api.themoviedb.org/3/movie/76341?api_key=ac202904369986b675f1700a286c33f6')
       .then(response => response.json())
       .then(json => {
+        setFilms(json)
         console.log(json)
-        setUsers(json)
       })
   }, [])
 
   return (
     <>
       <h1>Favourite Movies</h1>
-      {/* {todo && <h2>{todo.title}</h2>} */}
-      {
-        users && users.map(user => {
-          return (
-            <>
-              <h2 key={user.name}>{user.name}</h2>
-              <p key={user.email}>{user.email}</p>
-            </>
-          )
-        })
-      }
+      <div>{films.title}</div>
+      <img src={`https://image.tmdb.org/t/p/w500/${films.poster_path}`} alt={films.id} />
+      <p>{films.overview}</p>
     </>
   )
 }
 
 export default App;
+
+// Зображення: base_url/file_size/file_path
+// base_url : 'https://image.tmdb.org/'
+// file_size : 't/p/w500'
+// file_path: 
+
+// https://api.themoviedb.org/3/movie/76341?api_key=ac202904369986b675f1700a286c33f6
+
+// https://api.themoviedb.org/3/movie/550?api_key=ac202904369986b675f1700a286c33f6
+
+// https://www.themoviedb.org/3/movie/76341?api_key=ac202904369986b675f1700a286c33f6
