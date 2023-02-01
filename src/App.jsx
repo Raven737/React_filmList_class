a88d52a361dc985e30b99c1fed9c9b58a0fb5553
Raven737 <prvm79@gmail.com>
2023-02-01 13:54:53 +0200

    111

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { render } from '@testing-library/react';
 import { useState, useEffect } from 'react';
 
 const App = () => {
@@ -6,7 +5,7 @@ const App = () => {
   const [films, setFilms] = useState();
 
   useEffect(() => {
-    fetch('https://api.themoviedb.org/3/movie/76341?api_key=ac202904369986b675f1700a286c33f6')
+    fetch('https://api.themoviedb.org/3/movie/314?api_key=ac202904369986b675f1700a286c33f6')
       .then(response => response.json())
       .then(json => {
         setFilms(json)
@@ -15,24 +14,13 @@ const App = () => {
   }, [])
 
   return (
-    <>
-      <h1>Favourite Movies</h1>
-      <div>{films.title}</div>
-      <img src={`https://image.tmdb.org/t/p/w500/${films.poster_path}`} alt={films.id} />
+    <div className="film-wrap">
+      <h1 className="headline">Favourite Movies</h1>
+      <h2>{films.title}</h2>
+      <img className="film-img" src={`https://image.tmdb.org/t/p/w500/${films.poster_path}`} alt={films.id} />
       <p>{films.overview}</p>
-    </>
+    </div>
   )
 }
 
 export default App;
-
-// Зображення: base_url/file_size/file_path
-// base_url : 'https://image.tmdb.org/'
-// file_size : 't/p/w500'
-// file_path: 
-
-// https://api.themoviedb.org/3/movie/76341?api_key=ac202904369986b675f1700a286c33f6
-
-// https://api.themoviedb.org/3/movie/550?api_key=ac202904369986b675f1700a286c33f6
-
-// https://www.themoviedb.org/3/movie/76341?api_key=ac202904369986b675f1700a286c33f6
