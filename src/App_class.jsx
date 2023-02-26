6b2fb4c68529cb399297af695a1098fc28951f90
Raven737 <prvm79@gmail.com>
2023-02-26 22:06:13 +0200

    added fn select pages

--- src/App_class.jsx
+++ src/App_class.jsx
@@ -9,17 +9,9 @@ class App_class extends Component {
             films: null,
             error: null,
             isLoading: true,
-            page: 9,
+            page: 1,
         };
     }
-
-    pageDown = () => {
-        this.setState({ page: this.state.page - 1 });
-    };
-
-    pageUp = () => {
-        this.setState({ page: this.state.page + 1 });
-    };
 
     componentDidMount() {
         let url = `https://api.themoviedb.org/3/discover/movie?api_key=ac202904369986b675f1700a286c33f6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${this.state.page}&with_watch_monetization_types=flatrate`;
@@ -33,8 +25,27 @@ class App_class extends Component {
             });
     }
 
+    // pageDown = () => {
+    //     this.setState({ page: this.state.page - 1 });
+    // };
+    // pageUp = () => {
+    //     this.setState({ page: this.state.page + 1 });
+    // };
+    // pageNumber1 = () => {
+    //     this.setState({ page: 1 });
+    // };
+    // pageNumber2 = () => {
+    //     this.setState({ page: 2 });
+    // };
+    // pageNumber3 = () => {
+    //     this.setState({ page: 3 });
+    // };
+    // lastPage = () => {
+    //     this.setState({ page: this.films?.total_pages });
+    // };
+
     componentDidUpdate(prevProps, prevState) {
-        if (prevState !== this.state.page) {
+        if (prevState.page !== this.state.page) {
             let url = `https://api.themoviedb.org/3/discover/movie?api_key=ac202904369986b675f1700a286c33f6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${this.state.page}&with_watch_monetization_types=flatrate`;
             fetch(url)
                 .then((response) => response.json())
@@ -48,21 +59,48 @@ class App_class extends Component {
     }
 
     render() {
-        const { films, error, isLoading, page } = this.state;
-        // console.log(this.state.page);
+        // const { films, error, isLoading, page } = this.state;
+
+        const pageDown = () => {
+            this.setState({ page: this.state.page - 1 });
+        };
+        const pageUp = () => {
+            this.setState({ page: this.state.page + 1 });
+        };
+        const pageNumber1 = () => {
+            this.setState({ page: 1 });
+        };
+        const pageNumber2 = () => {
+            this.setState({ page: 2 });
+        };
+        const pageNumber3 = () => {
+            this.setState({ page: 3 });
+        };
+        const selectNumber = () => {
+            this.setState({ page: document.getElementById("enterBtn").value });
+        };
+        const lastPage = () => {
+            this.setState({ page: this.state.films?.total_pages });
+        };
         return (
             <div className="film-wrap">
                 <h1 className="headline">Favourite Movies</h1>
+                <h2 className="headline">page № {this.state.page}</h2>
                 <Pagination
-                    films={films}
-                    page={page}
-                    pageDown={this.pageDown}
-                    pageUp={this.pageUp}
+                    films={this.state.films}
+                    page={this.state.page}
+                    pageDown={pageDown}
+                    pageUp={pageUp}
+                    pageNumber1={pageNumber1}
+                    pageNumber2={pageNumber2}
+                    pageNumber3={pageNumber3}
+                    selectNumber={selectNumber}
+                    lastPage={lastPage}
                 />
-                {error ? (
-                    <div>Error: {error.message}</div>
-                ) : !isLoading ? (
-                    films.results.map((film) => {
+                {this.state.error ? (
+                    <div>Error: {this.state.error.message}</div>
+                ) : !this.state.isLoading ? (
+                    this.state.films.results.map((film) => {
                         return (
                             <div key={film.id}>
                                 <h2>{film.title}</h2>
--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -5,27 +5,77 @@ class Pagination extends Component {
         super(props);
     }
 
+    prev = () => {
+        if (this.props.page === 1) return null;
+        this.props.pageDown();
+    };
+    // next = () => {
+    //     if (this.props.page === this.props.films?.total_pages) return null;
+    //     this.props.pageUp();
+    // };
+    // pageNumber1 = () => {
+    //     this.props.pageNumber1();
+    // };
+    // pageNumber2 = () => {
+    //     this.props.pageNumber2();
+    // };
+    // pageNumber3 = () => {
+    //     this.props.pageNumber3();
+    // };
+    // lastPage = () => {
+    //     this.props.lastPage();
+    // };
+
     render() {
+        // const {
+        //     pageDown,
+        //     pageUp,
+        //     pageNumber1,
+        //     pageNumber2,
+        //     pageNumber3,
+        //     selectNumber,
+        //     lastPage,
+        // } = this.props;
+
         const prev = () => {
             if (this.props.page === 1) return null;
             this.props.pageDown();
         };
-
         const next = () => {
             if (this.props.page === this.props.films?.total_pages) return null;
             this.props.pageUp();
+        };
+        const pageNum1 = () => {
+            this.props.pageNumber1();
+        };
+        const pageNum2 = () => {
+            this.props.pageNumber2();
+        };
+        const pageNum3 = () => {
+            this.props.pageNumber3();
+        };
+        const selectNum = () => {
+            this.props.selectNumber();
+        };
+        const lstPage = () => {
+            this.props.lastPage();
         };
 
         return (
             <div className="pagination">
                 <button onClick={prev}>prev</button>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>5</button>
-                <input type="number" placeholder="enter page number" />
-                <button>{this.props.films?.total_pages}</button>
+                <button onClick={pageNum1}>1</button>
+                <button onClick={pageNum2}>2</button>
+                <button onClick={pageNum3}>3</button>
+                <input
+                    id="enterBtn"
+                    type="number"
+                    placeholder="enter page number"
+                />
+                <button onClick={selectNum}>Enter number</button>
+                <button onClick={lstPage}>
+                    {this.props.films?.total_pages}
+                </button>
                 <button onClick={next}>next</button>
             </div>
         );
