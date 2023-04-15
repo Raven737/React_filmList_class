7d7993bff34977959df8bc344c7ce583e6585567
Raven737 <prvm79@gmail.com>
2023-04-15 11:04:34 +0300

    change

--- src/PopUp.jsx
+++ src/PopUp.jsx
@@ -1,6 +1,13 @@
 import { Component } from "react";
+import ThemeContext from "./Context";
 
 class PopUp extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+
+    //     };
+    // }
     render() {
         return (
             <div className="filmReleaseDate">
